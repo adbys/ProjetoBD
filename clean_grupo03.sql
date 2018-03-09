bc806464cfa2cb69c07daa5e09cfb34673b095cd
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-03-09 11:25:35 -0300

    Renomeando arquivos e adicionando cabeçalho

---
+++ clean_grupo03.sql
@@ -0,0 +1,20 @@
+--CLEAN GRUPO-03
+--ADBYS JOSÉ VASCONCELOS DE ANDRADE - 116110498
+--ARIANN MICHAEL MARTINS DE ANDRADE FARIAS - 115210534
+--LUAN ROCHA SILVA - 115210869
+--NILTON MANUEL NOGUEIRA GINANI - 115211281
+--VICTOR EDUARDO BORGES DE ARAUJO - 115210597
+--YOVANY MARROQUIN DA CUNHA - 115210445
+
+drop table ACUDE cascade constraints;
+drop table BACIA cascade constraints;
+drop table CONTRIB_P_PLUVIOMETRICO_ACUDE cascade constraints;
+drop table COTA_AREA_VOLUME cascade constraints;
+drop table ESTACAO_DE_QUALIDADE cascade constraints;
+drop table MEDICAO_COTA_DIARIA cascade constraints;
+drop table MEDICAO_PLUVIOMETRICA cascade constraints;
+drop table POSTO_PLUVIOMETRICO cascade constraints;
+drop table RIO cascade constraints;
+drop table TELEFONES_USUARIO cascade constraints;
+drop table USUARIO cascade constraints;
+drop table V_DIARIOS_MED_PLUVIOMETRICA cascade constraints;
